219e73cdffd8ad2c7e4ccfc06c4b9a864b7d30bf
Braden MacDonald <bmacdonald@mstar-ca.com>
2011-08-08 16:30:39 -0700

    Initial import of zbar for Marmalade, without any changes to zbar source files.

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef ZBAR_CONFIG_H
+#define ZBAR_CONFIG_H
+
+#define ZBAR_VERSION_MAJOR 0
+#define ZBAR_VERSION_MINOR 10
+#define ENABLE_QRCODE 1
+
+#define 	PRIx32   "x"
+
+//#ifndef __cplusplus
+//	#define inline __inline
+//#endif
+
+#define HAVE_LIBPTHREAD
+
+#endif
