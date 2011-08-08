f90fbefca051c0c0f0fbb00d6d11543b9fff0d3f
Braden MacDonald <bmacdonald@mstar-ca.com>
2011-08-08 16:41:54 -0700

    Removed C++ wrapper to fix compilation errors and avoid the use of exceptions, which generally aren't used in Marmalade projects.

--- src/zbar.h
+++ src/zbar.h
@@ -65,14 +65,6 @@
  *   extracts barcodes from a stream of bar and space widths
  */
 
-#ifdef __cplusplus
-
-/** C++ namespace for library interfaces */
-namespace zbar {
-    extern "C" {
-#endif
-
-
 /** @name Global library interfaces */
 /*@{*/
 
@@ -1293,20 +1285,5 @@ extern unsigned zbar_scanner_get_edge(const zbar_scanner_t *scn,
 extern zbar_color_t zbar_scanner_get_color(const zbar_scanner_t *scanner);
 
 /*@}*/
-
-#ifdef __cplusplus
-    }
-}
-
-# include "zbar/Exception.h"
-# include "zbar/Decoder.h"
-# include "zbar/Scanner.h"
-# include "zbar/Symbol.h"
-# include "zbar/Image.h"
-# include "zbar/ImageScanner.h"
-# include "zbar/Video.h"
-# include "zbar/Window.h"
-# include "zbar/Processor.h"
-#endif
 
 #endif
