01e58c9a29d0b87d7e269d214c979b7018a11a3a
Braden MacDonald <bmacdonald@mstar-ca.com>
2011-08-08 17:30:04 -0700

    Fixed linking error because of missing inline function.

--- src/zbar/symbol.cpp
+++ src/zbar/symbol.cpp
@@ -209,7 +209,7 @@ zbar_symbol_set_t *_zbar_symbol_set_create ()
     return(syms);
 }
 
-inline void _zbar_symbol_set_free (zbar_symbol_set_t *syms)
+void _zbar_symbol_set_free (zbar_symbol_set_t *syms)
 {
     zbar_symbol_t *sym, *next;
     for(sym = syms->head; sym; sym = next) {
